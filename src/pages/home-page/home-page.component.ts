818be2a34a30158d7f489bd97172f1c16df5cbc1
Erlan-Bam <erlanzh.gg@gmail.com>
2024-11-13 20:21:43 +0500

    feat: breakpoints init

---
+++ src/pages/home-page/home-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.module.scss',
+})
+export class HomePageComponent {}
